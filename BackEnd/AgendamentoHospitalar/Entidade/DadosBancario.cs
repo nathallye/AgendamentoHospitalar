dba4bba7ae17569e4418aec1e88e8dc500e656e2
Francesly Hebe <f.francesly@gmail.com>
2022-12-20 14:16:23 -0300

    Update API

---
+++ BackEnd/AgendamentoHospitalar/Entidade/DadosBancario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendamentoHospitalar.Entidade
+{
+    public partial class DadosBancario
+    {
+        [Key]
+        public int IdDadosBancarios { get; set; }
+        [StringLength(4)]
+        [Unicode(false)]
+        public string NumeroBanco { get; set; } = null!;
+        [StringLength(200)]
+        [Unicode(false)]
+        public string? CodigoPix { get; set; }
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? Agencia { get; set; }
+        [StringLength(30)]
+        [Unicode(false)]
+        public string? NumeroConta { get; set; }
+        public bool? Poupanca { get; set; }
+        [Column("idProfissional")]
+        public int IdProfissional { get; set; }
+
+        [ForeignKey("IdProfissional")]
+        [InverseProperty("DadosBancarios")]
+        public virtual Profissional IdProfissionalNavigation { get; set; } = null!;
+    }
+}
--- BackEnd/AgendamentoHospitalar/Program.cs
+++ BackEnd/AgendamentoHospitalar/Program.cs
@@ -6,6 +6,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AgendamentoHospitalar.Context.DatabaseContext>();
+builder.Services.AddScoped<AgendamentoHospitalar.Interface.IProfissionalRepository, AgendamentoHospitalar.Repository.ProfissionalRepository>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("RegraCors",
+    policy =>
+    {
+        policy.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod();
+
+
+
+    });
+});
+
 
 var app = builder.Build();
 
