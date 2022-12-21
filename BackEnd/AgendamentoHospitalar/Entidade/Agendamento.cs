1f17975c7e4391ddb524eb9960fb69b95fe00ce0
Nathallye Bacelar <nathallyet@gmail.com>
2022-12-21 14:26:50 -0300

    implementação das navegações no controller agendamentos

--- BackEnd/AgendamentoHospitalar/Entidade/Agendamento.cs
+++ BackEnd/AgendamentoHospitalar/Entidade/Agendamento.cs
@@ -1,5 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
