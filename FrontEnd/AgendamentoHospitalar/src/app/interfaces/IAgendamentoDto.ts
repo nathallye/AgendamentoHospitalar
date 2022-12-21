0d7081240c55627097c6cb9ea7b9a5c42fd8cc51
Nathallye Bacelar <nathallyet@gmail.com>
2022-12-21 15:57:27 -0300

    telas agendamento

---
+++ FrontEnd/AgendamentoHospitalar/src/app/interfaces/IAgendamentoDto.ts
@@ -0,0 +1,18 @@
+import { IBeneficiarioDto } from "./IBeneficiarioDto";
+import IEspecialidadeDTO from "./IEspecialidadeDTO";
+import { IHospitalDto } from "./IHospitalDto";
+import { IProfissionalDto } from "./IProfissionalDto";
+
+export interface IAgendamentoDto {
+  idAgendamento: number,
+  idBeneficiario: number,
+  idHospital: number,
+  idEspecialidade: number,
+  idProfissional: number,
+  dataHoraAgendamento: Date,
+  ativo: boolean,
+  idBeneficiarioNavigation: IBeneficiarioDto,
+  idHospitalNavigation: IHospitalDto,
+  idEspecialidadeNavigation: IEspecialidadeDTO,
+  idProfissionalNavigation: IProfissionalDto
+}
--- FrontEnd/AgendamentoHospitalar/src/app/interfaces/IHospitalDto.ts
+++ FrontEnd/AgendamentoHospitalar/src/app/interfaces/IHospitalDto.ts
@@ -1,9 +1,9 @@
 export interface IHospitalDto {
-    idHospital: number,
-    nome: string,
-    cnpj: string,
-    endereco: string,
-    telefone: string,
-    cnes: string,
-    ativo: boolean,
-  }
+  idHospital: number,
+  nome: string,
+  cnpj: string,
+  endereco: string,
+  telefone: string,
+  cnes: string,
+  ativo: boolean,
+}
