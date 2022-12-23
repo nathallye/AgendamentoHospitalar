e1f4085c3cd39dc06227b497661d896dfd0dd4ae
Francesly Hebe <f.francesly@gmail.com>
2022-12-22 22:45:11 -0300

    correcao NgModule

---
+++ FrontEnd/AgendamentoHospitalar/src/app/components/agendamentoconfig/detalhe-agendamentoconfig/detalhe-agendamentoconfig.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendamento-detalhe',
+  templateUrl: './detalhe-agendamentoconfig.component.html',
+  styleUrls: ['./detalhe-agendamentoconfig.component.css']
+})
+export class DetalheAgendamentoconfigComponent {
+
+}
